fa9be55ef96f5b4478d2d62daa7cdfb827f2281a
Uzytkownik <martinpes2009@gmail.com>
2017-03-23 00:09:37 +0100

    added new task nr 2

---
+++ src/assets/gjson.js
@@ -0,0 +1,17 @@
+const Szczecin =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+      "name": "Szczecin"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.628295898437498,
+          53.38988075156031
+        ]
+      }
+    },
